5c2117f69939b9d06abb17968fbe88d9e05ec5a6
team3131-dev <dev@gladstone.team.com>
2019-02-02 11:13:15 -0800

    delete useless print statement

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -67,7 +67,6 @@ public class Robot extends TimedRobot {
 		
 	}
 	public void teleopPeriodic() {
-    System.out.print("teleopPeriodic");
 		teleopManipulatorPeriodic();
 		teleopDrivePeriodic();
 		doubleSolenoidControl();
